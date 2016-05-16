2ccfc56893887d1624a008493e7c3ffd2cf020d7
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-17 05:03:53 +0600

    change nginx.conf location

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-/etc/init.d/nginx restart
-#sudo ln -s /home/box/stepic/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart

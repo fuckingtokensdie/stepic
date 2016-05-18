6875f97bd4959669c87d98f142182170ef041407
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-18 10:34:43 +0600

    items is method

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from cgi import parse_qs
 
 def wsgi_app(environment,response_handler):
     query = parse_qs(environment['QUERY_STRING'])
-    response_body = ['{}={}\r\n'.format(k, v) for k, v in query.items]
+    response_body = ['{}={}\r\n'.format(k, v) for k, v in query.items()]
     response_handler('200 OK', [('Content-Type', 'text/plain')])
     return response_body

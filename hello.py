ac27dee695ace65fdf5075c9ed32a6cd45ec3740
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-18 10:44:17 +0600

    change parameter parser logic

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 from cgi import parse_qs
 
 def wsgi_app(environment,response_handler):
-    query = parse_qs(environment['QUERY_STRING'])
-    response_body = ['{}={}\r\n'.format(k, v) for k, v in query.items()]
+    parameter = parse_qs(environment['QUERY_STRING'])
+    response_body = []
+    for parameter_key, parameter_value_list in parameter.items():
+        for parameter_value in parameter_value_list:
+            line = '{}={}\r\n'.format(parameter_key, parameter_value)
+            response_body.append(line)
     response_handler('200 OK', [('Content-Type', 'text/plain')])
     return response_body

91f87ba411384b7c898065e44e6085637efcef16
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-18 10:01:33 +0600

    separate gunicorn config

--- hello.py
+++ hello.py
@@ -1,17 +1,5 @@
 from cgi import parse_qs
 
-CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--workers=16',
-        '--timeout=60',
-        'hello.wsgi_app',
-        )
-}
-
 def wsgi_app(environment,response_handler):
     query = parse_qs(environment['QUERY_STRING'])
     response_body = ['{}={}\r\n'.format(key, value) for key, value in query]

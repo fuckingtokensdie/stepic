714a7ec457e9b5c3f75616306b7f24f0ba5f8848
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-18 11:09:30 +0600

    keep blank values in query string

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from cgi import parse_qs
 
 def wsgi_app(environment,response_handler):
-    parameter = parse_qs(environment['QUERY_STRING'])
+    parameter = parse_qs(environment['QUERY_STRING'], keep_blank_values=True)
     response_body = []
     for parameter_key, parameter_value_list in parameter.items():
         for parameter_value in parameter_value_list:

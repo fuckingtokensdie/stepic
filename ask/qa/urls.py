2e05ca99937b3250f9514b7c789c42f40edede6c
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-20 10:26:47 +0600

    fix typo

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from views import not_found, test
 
 urlpatterns = patterns('',
-    url(r'^/$', not_found'),
+    url(r'^/$', not_found),
     url(r'^login/$', not_found),
     url(r'^signup/$', not_found),
     url(r'^ask/$', not_found),

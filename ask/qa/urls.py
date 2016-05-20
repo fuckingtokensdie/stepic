3711c2494f39ea4dd26a3f1ce554a11abc8a9034
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-20 10:31:07 +0600

    return 200 on /

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from views import not_found, test
 
 urlpatterns = patterns('',
-    url(r'^/$', test),
+    url(r'^$', test),
     url(r'^login/$', test),
     url(r'^signup/$', test),
     url(r'^ask/$', test),

fb6e27f49d9cd662ba9d2e90d5bc2690cca5f8b3
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-20 09:41:30 +0600

    change backend server port

--- etc/hello.py
+++ etc/hello.py
@@ -3,7 +3,7 @@ CONFIG = {
     'working_dir': '/home/box/web/ask/ask',
     'python': '/usr/bin/python',
     'args': (
-        '--bind=0.0.0.0:8080',
+        '--bind=0.0.0.0:8000',
         '--workers=16',
         '--timeout=60',
         '--log-file=/home/box/web/logs/gunicorn.log',

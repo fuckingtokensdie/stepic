8f48627ee1fdd3671acc9389abedee28d9d3679a
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-20 10:35:37 +0600

    change url pattern

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,10 +3,10 @@ from views import not_found, test
 
 urlpatterns = patterns('',
     url(r'^$', test),
-    url(r'^login/$', test),
-    url(r'^signup/$', test),
-    url(r'^ask/$', test),
-    url(r'^popular/$', test),
-    url(r'^new/$', test),
-    url(r'^question/(.+)/$', test),
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^ask/', test),
+    url(r'^popular/', test),
+    url(r'^new/', test),
+    url(r'^question/(.+)/', test),
 )

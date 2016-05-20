9e6a4b20a064d08b7807b8500dd463d6440ea2ec
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-20 09:37:46 +0600

    deploy servers on django-mode

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,12 @@
 CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
+    'mode': 'django',
+    'working_dir': '/home/box/web/ask/ask',
     'python': '/usr/bin/python',
     'args': (
         '--bind=0.0.0.0:8080',
         '--workers=16',
         '--timeout=60',
         '--log-file=/home/box/web/logs/gunicorn.log',
-        'hello:wsgi_app',
+        'settings',
     )
 }

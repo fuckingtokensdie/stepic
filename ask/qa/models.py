2747a55f2b1fa11a44729c3afff0bb29dea440f1
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-22 04:33:38 +0600

    default values in Question model fields

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,9 +5,9 @@ class Question(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User)
-    likes = models.ManyToManyField(User, related_name='question_likes')
+    likes = models.ManyToManyField(User, related_name='question_likes', blank=True)
 
 class Answer(models.Model):
     text = models.TextField()

b1513e61f85546d460bcad4fe80b0f24b9ed9f48
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-22 04:05:38 +0600

    add models to ask app

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)

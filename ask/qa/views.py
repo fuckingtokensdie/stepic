47f30a8a5faf4896c73874904c07081097cd068a
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-22 16:32:54 +0600

    popular question

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -16,8 +16,19 @@ def main(request):
         questions = paginator.page(paginator.num_pages)
     return render(request, 'qa/main.html', {'questions': questions})
 
+
 def popular(request):
-    return HttpResponse('OK')
+    all_questions = Question.object.order_by('-rating')
+    paginator = Paginator(all_questions, 10)
+    page = request.GET.get('page', 1)
+    try:
+        questions = paginator.page(page)
+    except PageNotAnInteger:
+        questions = paginator.page(1)
+    except EmptyPage:
+        questions = paginator.page(paginator.num_pages)
+    return render(request, 'qa/main.html', {'questions': questions})
+
 
 def question(request, question_id):
     return HttpResponse('OK')

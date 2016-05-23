46baf0eaa8d0d291af1986cc465d4c0ef71e22d4
Nurzhan Nazarov <nurzhan@tuta.io>
2016-05-23 19:06:31 +0600

    import exceptions

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from models import Question, Answer
 
 
